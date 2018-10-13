2ea5f0817e8db73f7b13979c8f19d2d5364008a4
Shivani Nanda <shivanin.nanda@gmail.com>
2018-10-13 10:02:41 -0400

    Initial commit

---
+++ Jeopardy.js
@@ -0,0 +1,351 @@
+var score = 0;
+
+function Riddle100() {
+    riddle100 = window.prompt("What is full of holes, but can contain a lot of water?");
+    if (riddle100 == "Sponge") {
+        window.alert("That is correct you have earned 100 points!");
+        score=score + 100;
+        window.alert("Your score is " + score + ".");      
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is a sponge.");
+        window.alert("Your score is " + score + ".");  
+    }
+}
+
+
+function Riddle200() {
+    riddle200 = window.prompt("What has a face and two hands, but no legs?");
+    if (riddle200 == "Clock") {
+        window.alert("That is correct you have earned 200 points!");
+        score=score + 200;
+        window.alert("Your score is " + score + "."); 
+    }
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is a clock.");
+        window.alert("Your score is " + score + ".");  
+    }
+}
+
+function Riddle300() {
+    riddle300 = window.prompt("What had a neck but no head?");
+    if (riddle300 == "Bottle") {
+        window.alert("That is correct you have earned 300 points!");
+        score=score + 300;
+        window.alert("Your score is " + score + ".");
+    }
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is a bottle.");
+        window.alert("Your score is " + score + ".");  
+    }
+}
+
+function Riddle400() {
+    riddle400 = window.prompt("What has to be broken before you can use it?");
+    if (riddle400 == "Egg") {
+        window.alert("That is correct you have earned 400 points!");
+        score=score + 400;
+        window.alert("Your score is " + score + ".");    
+    }
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is an egg.");
+        window.alert("Your score is " + score + ".");  
+    }
+}
+
+function Riddle500() {
+    riddle500 = window.prompt("I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost everybody.");
+    if (riddle500 == "Lead") {
+        window.alert("That is correct you have earned 500 points!");
+        score=score + 500;
+        window.alert("Your score is " + score + ".");
+        
+    }
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is lead.");
+        window.alert("Your score is " + score + ".");  
+    }
+}
+
+function Math100() {
+    math100 = window.prompt("Simplify a(b+c):");
+    if (math100 == "ab+ac") {
+        window.alert("That is correct you have earned 100 points!");
+        score=score + 100;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is ab+ac.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Math200() {
+    math200 = window.prompt("How many centimeters are in a meter?");
+    if (math200 == "100") {
+        window.alert("That is correct you have earned 200 points!");
+        score=score + 200;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 100.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Math300() {
+    math300 = window.prompt("What is the difference between the LCM (Least Common Multiple) and the GCF (Greatest Common Factor) of 5, 10, and 65?");
+    if (math300 == "125") {
+        window.alert("That is correct you have earned 300 points!");
+        score=score + 300;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 125.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Math400() {
+    math400 = window.prompt("In an All-Area track meet, 216 sprinters enter a 100 meter dash competition. The track has 6 lanes, so only 6 sprinters can compete at a time. At the end of each race, the five non-winners are eliminated, and the winner will compete again in a later race. How many races are needed to determine the champion sprinter?");
+    if (math400 == "43") {
+        window.alert("That is correct you have earned 400 points!");
+        score=score + 400;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 43.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Math500() {
+    math500 = window.prompt("A box contains 3 red chips and 2 green chips. Chips are drawn randomly, one at a time without replacement, until all 3 of the reds are drawn or until both green chips are drawn. What is the probability that the 3 reds are drawn??");
+    if (math500 == "2/5") {
+        window.alert("That is correct you have earned 500 points!");
+        score=score + 500;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 2/5.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function ComputerScience100() {
+    compsciconnect100 = window.prompt("What is the binary number for 100?");
+    if (compsciconnect100 == "1100100") {
+        window.alert("That is correct you have earned 100 points!");
+        score=score + 100;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 1100100.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function ComputerScience200() {
+    compsciconnect200 = window.prompt("What is the number in 10's digit for this binary number: 101010?");
+    if (compsciconnect200 == "42") {
+        window.alert("That is correct you have earned 200 points!");
+        score=score + 200;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 42.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function ComputerScience300() {
+    compsciconnect300 = window.prompt("When is the application for next summer due?");
+    if (compsciconnect300 == "March 31, 2018") {
+        window.alert("That is correct you have earned 300 points!");
+        score=score + 300;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is March 31, 2017.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function ComputerScience400() {
+    compsciconnect400 = window.prompt("Is the syntax of this line correct (this is Python, answer (Y/N)): for eachPass in range (4)");
+    if (compsciconnect400 == "N") {
+        window.alert("That is correct you have earned 400 points!");
+        score=score + 400;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is N.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function ComputerScience500() {
+    compsciconnect500 = window.prompt("Is the syntax of this line correct (this is JavaScript, answer (Y/N)): if compsciconnect500 = answer: ");
+    if (compsciconnect500 == "N") {
+        window.alert("That is correct you have earned 500 points!");
+        score=score + 500;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is N.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Science100() {
+    science100 = window.prompt("What does a human being breathe to stay alive?");
+    if (science100 == "Oxygen") {
+        window.alert("That is correct you have earned 100 points!");
+        score=score + 100;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is N.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Science200() {
+    science200 = window.prompt("What is the 2nd element in the Periodic Table?");
+    if (science200 == "Helium") {
+        window.alert("That is correct you have earned 200 points!");
+        score=score + 200;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is helium.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Science300() {
+    science300 = window.prompt("What does DNA stand for?");
+    if (science300 == "Deoxyribo Nucleic Acid") {
+        window.alert("That is correct you have earned 300 points!");
+        score=score + 300;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is Deoxyribo Nucleic Acid.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Science400() {
+    science400 = window.prompt("How many valence electrons are there in the element Carbon?");
+    if (science400 == "4") {
+        window.alert("That is correct you have earned 400 points!");
+        score=score + 400;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is 4.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Science500() {
+    science500 = window.prompt("What is the formula of Glucose?");
+    if (science500 == "C6H12O6") {
+        window.alert("That is correct you have earned 500 points!");
+        score=score + 500;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is C6H12O6.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Animals100() {
+    animals100 = window.prompt("Is a shark a fish or a mammal?");
+    if (animals100 == "Fish") {
+        window.alert("That is correct you have earned 100 points!");
+        score=score + 100;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is fish.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Animals200() {
+    animals200 = window.prompt("Bunnies are born blind. True or False?");
+    if (animals200 == "True") {
+        window.alert("That is correct you have earned 200 points!");
+        score=score + 200;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is true.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Animals300() {
+    animals100 = window.prompt("What is the largest 'Big Cat' in the world?");
+    if (animals100 == "Tiger") {
+        window.alert("That is correct you have earned 300 points!");
+        score=score + 300;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is tiger.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Animals400() {
+    animals400 = window.prompt("Red pandas are related to Giant Red Panda. True or False?");
+    if (animals400 == "False") {
+        window.alert("That is correct you have earned 400 points!");
+        score=score + 400;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is false.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+function Animals500() {
+    animals500 = window.prompt("What is the world's largest species of spider?");
+    if (animals500 == "Tarantula") {
+        window.alert("That is correct you have earned 500 points!");
+        score=score + 500;
+        window.alert("Your score is " + score + ".");
+    }
+    
+    else {
+        window.alert("Sorry, that is incorrect, the correct answer is tarantula.");
+        window.alert("Your score is " + score + ".");
+    }
+}
+
+
